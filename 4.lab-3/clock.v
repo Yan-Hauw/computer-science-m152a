df71dc92295dc45c83c790f414450eb7994143b6
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2023-05-02 13:33:10 -0800

    file from week 5 Tuesday

---
+++ 4.lab-3/clock.v
@@ -0,0 +1,149 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:14:32 05/02/2023 
+// Design Name: 
+// Module Name:    clock 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clock(
+   // Outputs
+   RsTx, led,
+   // Inputs
+   RsRx, sw, btnS, btnR, clk
+   );
+  //include "seq_definitions.v"
+   
+   // USB-UART
+   input        RsRx;
+   output       RsTx;
+
+   // Misc.
+   input  [7:0] sw;
+   output [7:0] led;
+   input        btnS;                 // single-step instruction
+   input        btnR;                 // arst
+   
+   // Logic
+   input        clk;                  // 100MHz
+   
+   /*AUTOWIRE*/
+   // Beginning of automatic wires (for undeclared instantiated-module outputs)
+   wire [seq_dp_width-1:0] seq_tx_data;         // From seq_ of seq.v
+   wire                 seq_tx_valid;           // From seq_ of seq.v
+   wire [7:0]           uart_rx_data;           // From uart_top_ of uart_top.v
+   wire                 uart_rx_valid;          // From uart_top_ of uart_top.v
+   wire                 uart_tx_busy;           // From uart_top_ of uart_top.v
+   // End of automatics
+   
+   wire        rst;
+   wire        arst_i;
+   reg [26:0] clk_dv_inc;
+
+   reg [1:0]   arst_ff;
+   reg [25:0]  clk_dv;
+   reg         clk_en;
+   reg         clk_en_d;
+      
+   reg [7:0]   inst_wd;
+   reg         inst_vld;
+   reg [2:0]   step_d;
+
+   reg [7:0]   inst_cnt;
+   
+   reg [26:0]  low_freq_clk_dv_inc;
+   reg [25:0]
+   
+   // ===========================================================================
+   // Asynchronous Reset
+   // ===========================================================================
+
+   assign arst_i = btnR;
+   assign rst = arst_ff[0];
+   
+   always @ (posedge clk or posedge arst_i)
+     if (arst_i)
+       arst_ff <= 2'b11;
+     else
+       arst_ff <= {1'b0, arst_ff[1]};
+
+   // ===========================================================================
+   // 763Hz timing signal for clock enable --> 381 Hz
+   // ===========================================================================
+
+   //assign clk_dv_inc = clk_dv + 1;
+   
+   always @ (posedge clk)
+     clk_dv_inc <= clk_dv + 1;
+     if (rst)
+       begin
+          clk_dv   <= 0;
+          clk_en   <= 1'b0;
+          clk_en_d <= 1'b0;
+       end
+     else
+       begin
+          clk_dv   <= clk_dv_inc[17:0];
+          clk_en   <= clk_dv_inc[18];
+          clk_en_d <= clk_en;
+       end
+   
+   // ===========================================================================
+   // Instruction Stepping Control
+   // ===========================================================================
+
+   always @ (posedge clk)
+     if (rst)
+       begin
+          inst_wd[7:0] <= 0;
+          step_d[2:0]  <= 0;
+       end
+     else if (clk_en)
+       begin
+          inst_wd[7:0] <= sw[7:0];
+          step_d[2:0]  <= {btnS, step_d[2:1]};
+       end
+
+   always @ (posedge clk)
+     if (rst)
+       inst_vld <= 1'b0;
+     else
+       inst_vld <= ~step_d[0] & step_d[1] & clk_en_d;
+
+   always @ (posedge clk)
+     if (rst)
+       inst_cnt <= 0;
+     else if (inst_vld)
+       inst_cnt <= inst_cnt + 1;
+
+   assign led[7:0] = inst_cnt[7:0];
+   
+   // ===========================================================================
+   // Sequencer
+   // ===========================================================================
+
+   seq seq_ (// Outputs
+             .o_tx_data                 (seq_tx_data[seq_dp_width-1:0]),
+             .o_tx_valid                (seq_tx_valid),
+             // Inputs
+             .i_tx_busy                 (uart_tx_busy),
+             .i_inst                    (inst_wd[seq_in_width-1:0]),
+             .i_inst_valid              (inst_vld),
+             /*AUTOINST*/
+             // Inputs
+             .clk                       (clk),
+             .rst                       (rst));
+
+endmodule
